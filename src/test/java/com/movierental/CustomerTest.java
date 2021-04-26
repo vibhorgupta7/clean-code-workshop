af0cca5347f71a668ff14f422b1ac3082acb8f50
vibhorgupta7 <vibhor4k@gmail.com>
2021-04-26 21:08:54 +0530

    First group of test cases added with Extraction of amountFor method

--- src/test/java/com/movierental/CustomerTest.java
+++ src/test/java/com/movierental/CustomerTest.java
@@ -2,8 +2,22 @@ package com.movierental;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class CustomerTest {
     @Test
-    public void test(){}
+    public void verifyThatTheGeneratedStatementIsCorrect(){
+        Customer customer = new Customer("Vibhor");
+        customer.addRental(new Rental(new Movie("GodFather", Movie.REGULAR),3));
+        customer.addRental(new Rental(new Movie("Toy Story", Movie.CHILDRENS),2));
+        customer.addRental(new Rental(new Movie("Avengers", Movie.NEW_RELEASE),4));
+
+        assertEquals("Rental Record for Vibhor\n" +
+                "\tGodFather\t3.5\n" +
+                "\tToy Story\t1.5\n" +
+                "\tAvengers\t12.0\n" +
+                "Amount owed is 17.0\n" +
+                "You earned 4 frequent renter points", customer.statement());
+    }
 
 }
